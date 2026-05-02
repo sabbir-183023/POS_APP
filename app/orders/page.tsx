3ae24e6cd366789e78d56df7d387c85a393b0afb
sabbir-183023 <sabbir.us.183023@gmail.com>
2026-05-02 18:18:28 +0600

    Update import pat4yhh for OrderTable component

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,6 +1,6 @@
 
 import Link from 'next/link';
-import OrderTable from '../components/orders/OrderTable.tsx';
+import OrderTable from '../components/orders/OrderTable';
 
 
 export default function OrdersPage() {
